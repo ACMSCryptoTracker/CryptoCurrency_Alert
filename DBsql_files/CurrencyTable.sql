483dc9c1fb31626ebd3d51b4bfcfb03a04329d09
urjakotharo <urjakothari5@gmail.com>
2018-03-21 22:58:11 +0530

    Add Updated sql file

--- DBsql_files/CurrencyTable.sql
+++ DBsql_files/CurrencyTable.sql
@@ -15,4 +15,39 @@ CREATE TABLE public.currency
 	percentage_change_24h double precision,
 	percentage_change_7d double precision,
 	last_updated double precision
-)
+);
+CREATE OR REPLACE FUNCTION notify_event() RETURNS TRIGGER AS $$
+
+    DECLARE 
+        data json;
+        notification json;
+    
+    BEGIN
+    
+        -- Convert the old or new row to JSON, based on the kind of action.
+        -- Action = DELETE?             -> OLD row
+        -- Action = INSERT or UPDATE?   -> NEW row
+        IF (TG_OP = 'DELETE') THEN
+            data = row_to_json(OLD);
+        ELSE
+            data = row_to_json(NEW);
+        END IF;
+        
+        -- Contruct the notification as a JSON string.
+        notification = json_build_object(
+                          'table',TG_TABLE_NAME,
+                          'action', TG_OP,
+                          'data', data);
+        
+                        
+        -- Execute pg_notify(channel, notification)
+        PERFORM pg_notify('events',notification::text);
+        
+        -- Result is ignored since this is an AFTER trigger
+        RETURN NULL; 
+    END;
+    
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER currency_AFTER AFTER INSERT OR UPDATE ON currency FOR EACH ROW EXECUTE PROCEDURE notify_event();
+
+
